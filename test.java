33543a9c4a809306b10305da50f893fc6889840b
lime007 <ljinmyung@kakao.com>
2022-09-23 02:05:09 +0900

    Add test [Mul]

--- test.java
+++ test.java
@@ -4,7 +4,9 @@ public class test {
   int y = 4;
   int sum = x + y;
   int sub = x - y;
+  int mul = x * y;
   System.out.println(sum);
   System.out.println(sub);
+  System.out.println(mul);
  }
 }
