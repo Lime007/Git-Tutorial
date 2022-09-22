8d9c3b0a121fa6fce2f2a232c9d8176df2dea5e6
lime007 <ljinmyung@kakao.com>
2022-09-22 21:51:18 +0900

    Add test [Add]

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+ public static void main(String[] args) {
+  int x = 3;
+  int y = 4;
+  int sum = x + y;
+  int sub = x - y;
+  System.out.println(sum);
+  System.out.println(sub);
+ }
+}
